d5449c1445a4b6caaf7abc62c1d9fcae4a49049d
Hezi Nakash <hezi.hn@gmai.com>
2018-12-14 16:33:08 +0200

    Updates

---
+++ precache-manifest.790a446c7877af98f5e0bb67f3499eed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c38e43b7f12021c273bf6d96a9429cb0",
+    "url": "/DTracks/static/media/guitars.c38e43b7.jpg"
+  },
+  {
+    "revision": "bf745694ac40df8c1dd6068ac9f2101d",
+    "url": "/DTracks/static/media/Apple Music.bf745694.png"
+  },
+  {
+    "revision": "f71ffcebe7f4f43021b0",
+    "url": "/DTracks/static/js/runtime~main.f71ffceb.js"
+  },
+  {
+    "revision": "dc02b03f3711ea1aa259",
+    "url": "/DTracks/static/js/main.dc02b03f.chunk.js"
+  },
+  {
+    "revision": "072eefe32204f0a0abee",
+    "url": "/DTracks/static/js/1.072eefe3.chunk.js"
+  },
+  {
+    "revision": "dc02b03f3711ea1aa259",
+    "url": "/DTracks/static/css/main.00187b50.chunk.css"
+  },
+  {
+    "revision": "630063c2f9601e015c06507bc8b58c53",
+    "url": "/DTracks/index.html"
+  }
+];
